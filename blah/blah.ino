c093d2794414d528d5c9a885f20ef83db68dc30c
Lawrence Lee <lfqlee314@gmail.com>
2017-10-23 03:50:25 -0400

    add functionality to test file

---
+++ blah/blah.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  int a = 0;
+  char *string = "ethutah";
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int b = 0;
+  b += 1;
+}
